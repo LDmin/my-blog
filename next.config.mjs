c8e98df0a24f7632da8e6ab11fb38f0083d39a49
dongmin.lu <dongmin.lu@davyin.com>
2024-03-20 10:27:59 +0800

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
     ignoreDevErrors: true,
     ignoreBuildErrors: true
   },
+  transpilePackages: ["antd", "@ant-design", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table"],
   async redirects() {
     return [
       {
