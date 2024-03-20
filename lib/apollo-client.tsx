c190263a04e9cd8ab8bf2978008a2720d1bac211
dongmin.lu <dongmin.lu@davyin.com>
2024-03-20 13:00:55 +0800

    disabled cache

--- lib/apollo-client.tsx
+++ lib/apollo-client.tsx
@@ -2,7 +2,9 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "https://my-blog-wine-nine.vercel.app/api/graphql",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    resultCaching: false,
+  }),
 });
 
 export default client;
